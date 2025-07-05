deb1d22f45685d002740116dc1df84f32cd17634
Prasun3003 <prasunjha3003@gmail.com>
2025-07-06 04:38:26 +0530

    aijobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { Button } from "@/components/ui/button"
 import { useTRPC } from "@/trpc/client"
 import { useMutation } from "@tanstack/react-query"
 import { toast } from "sonner"
+import { Input } from "@/components/ui/input"
+import { useState } from "react"
 const Page = () => {
 
   const trpc = useTRPC()
@@ -16,10 +18,13 @@ const Page = () => {
     },
   }))
 
+  const [value, setValue] = useState("")
+
 
     return (
         <div className="p-4 max-w-7xl mx-auto">
-          <Button disabled={invoke.isPending} onClick={() => invoke.mutate({text: "prasunjha@gmail.com"})}>Invoke Background Job</Button>
+          <Input value={value} onChange={(e) => setValue(e.target.value)} />
+          <Button disabled={invoke.isPending} onClick={() => invoke.mutate({value: value})}>Invoke Background Job</Button>
         </div>
     )
 }
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,12 +1,27 @@
+import { Agent, openai, createAgent } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    await step.sleep("wait-a-moment", "10s");
-    await step.sleep("wait-a-moment", "10s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+
+    const writer = createAgent({
+        name: "writer",
+        system:`You are an expert Next.js developer.  You write readable, concise, maintainable, and performant code.
+        You are a senior software engineer with 10 years of experience in building production-grade web applications. You write simple Next js and React Js snippets 
+        `,
+        model: openai({ model: "gpt-4o" }),
+      });
+    
+      const {output} = await writer.run(
+        `Write the following snippets ${event.data.value}`,
+      )
+
+      console.log(output)
+    
+    
+    return { output };
   },
 );
