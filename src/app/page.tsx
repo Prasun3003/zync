a4bbbf69a330c359ac2acd4c419336fda9c48a29
Prasun3003 <prasunjha3003@gmail.com>
2025-07-05 17:42:07 +0530

    database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button"
 
-export default function Page() {
+import { prisma } from "@/lib/db"
+const Page = async () => {
+    const users = await prisma.user.findUnique({ where: { id: 1 } })
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24 text-3xl">
-            <h1 className="bg-orange-300 p-x-2 p-y-1 font-bold text-center flex items-center justify-between" >Home Page <Button variant="primary">Primary</Button></h1>
+        <main className="flex flex-col gap-2">
+            {users?.name}
         </main>
     )
 }
+export default Page
